bda1d6f68a22d63dfbc868c06a2d82879e3f5b4b
su-kun1899 <higedrum.coz@gmail.com>
2016-08-10 18:46:37 +0900

    必ずschemaspyディレクトリを階層作って出力する

--- src/main/java/com/higedrum/SchemaSpyConfigMap.java
+++ src/main/java/com/higedrum/SchemaSpyConfigMap.java
@@ -21,7 +21,7 @@ class SchemaSpyConfigMap {
     configMap.put(ParameterType.DB_NAME, config.getDbName());
     configMap.put(ParameterType.USER, config.getUser());
     configMap.put(ParameterType.CHARSET, config.getCharset());
-    configMap.put(ParameterType.OUTPUT_DIRECTORY, config.getOutputDirectory());
+    configMap.put(ParameterType.OUTPUT_DIRECTORY, config.getOutputDirectory() + "/schemaspy" );
   }
 
   public String put(final ParameterType key, final String value) {
