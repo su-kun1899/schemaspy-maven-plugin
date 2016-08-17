15b97e2428cf1ee168bb3607693deaaf38514159
su-kun1899 <higedrum.coz@gmail.com>
2016-08-17 16:16:29 +0900

    Stream Apiを使った処理に修正

--- src/main/java/red/sukun1899/SchemaSpyConfigParser.java
+++ src/main/java/red/sukun1899/SchemaSpyConfigParser.java
@@ -1,7 +1,17 @@
 package red.sukun1899;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static javafx.scene.input.KeyCode.R;
 
 /**
  * SchemaSpyに渡すパラメータ用のParser
@@ -22,28 +32,29 @@ class SchemaSpyConfigParser {
   }
 
   private List<String> toList(final SchemaSpyConfig config) {
-    List<String> configs = new ArrayList<>();
-    config.getConfigrations().forEach((key, value) -> {
-      switch (key) {
-        case OUTPUT_DIRECTORY:
-          value = value == null ? "target" : value;
-          value += "/schemaspy";
-          break;
-        default:
-          break;
-      }
+    return config.getConfigrations().entrySet().stream()
+        .map(e -> {
+          if (e.getKey().equals(ParameterType.OUTPUT_DIRECTORY)){
+            String value = e.getValue();
+            value = value == null ? "target" : value;
+            value += "/schemaspy";
+            e.setValue(value);
+          }
+          return e;
+        })
+        .map(e -> {
+          if (isLackOfRequiredValue(e.getKey(), e.getValue())) {
+            throw new IllegalArgumentException(e.getKey() + " is required. But value is empty.");
+          }
+          return e;
+        })
+        .filter(e -> e.getValue() != null)
+        .collect(ArrayList::new,
+            (list, entry) -> {
+              list.add(entry.getKey().getParameter());
+              list.add(entry.getValue());
+            }, ArrayList::addAll);
 
-      if (isLackOfRequiredValue(key, value)) {
-        throw new IllegalArgumentException(key + " is required. But value is empty.");
-      }
-
-      if (value != null) {
-        configs.add(key.getParameter());
-        configs.add(value);
-      }
-    });
-
-    return configs;
   }
 
   private boolean isLackOfRequiredValue(final ParameterType key, final String value) {
